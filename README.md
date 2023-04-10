1824a432129f1b8ee088da1ea33afd1c29f1fe88
Maximilian Ehlers <max@ehlers.berlin>
2023-04-10 18:15:29 +0200

    Adds back TLS support
    Signed-off-by: Maximilian Ehlers <max@ehlers.berlin>

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Simple web server in Rust which will return the callers IP address and runs on port 80.
 
+## Configuration
+
+If desired the server can be started with HTTPS.
+
+Set the environment variables `CERT_PATH` and `KEY_PATH` that point to the correct files necessary to set up TLS.
+
+## Logging
+`pretty_env_logger` is being used. Check out the docs on how to enable logging [here](https://docs.rs/pretty_env_logger/0.4.0/pretty_env_logger/#enable-logging).
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
+extern crate pretty_env_logger;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
     let ip = warp::filters::path::end()
         .and(warp::addr::remote())
         .map(|addr: Option<std::net::SocketAddr>| format!("{}", addr.unwrap()));
 
-    warp::serve(ip).run(([0, 0, 0, 0], 80)).await;
+    let cert_path: Option<String> = match std::env::var("CERT_PATH") {
+        Ok(path) => Some(path),
+        _ => None,
+    };
+    let key_path: Option<String> = match std::env::var("KEY_PATH") {
+        Ok(path) => Some(path),
+        _ => None,
+    };
+    if cert_path.and(key_path).is_some() {
+        warp::serve(ip)
+            .tls()
+            .cert_path(std::env::var("CERT_PATH").unwrap())
+            .key_path(std::env::var("KEY_PATH").unwrap())
+            .run(([0, 0, 0, 0], 443))
+            .await;
+    } else {
+        warp::serve(ip).run(([0, 0, 0, 0], 80)).await;
+    }
 }
